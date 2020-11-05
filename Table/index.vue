a8d5249b7cd6f5ed492401ecc245a517c99036a3
王占领 <wangzhanling@ctfo.com>
2020-11-05 20:02:41 +0800

    table支持分页

--- Table/index.vue
+++ Table/index.vue
@@ -1,47 +1,61 @@
 <template>
-   <el-table :row-class-name="rowClassName" :border="border" :data="tableData"
-      @sort-change="orderMethod" :show-header="showHeader" :height="height">
-      <template v-for="(item) in columns">
-         <template v-if="item.render">
-            <el-table-column v-if="item.renderHeader"
-               :prop="item.key"
-               :sortable="item.sortable?item.sortable:false"
-               :key="item.key" :align="item.align"
-               :width="item.width"
-               :render-header="item.renderHeader"
-               :class-name="item.className">
-               <template slot-scope="params">
-                  <v-slot :render="item.render" :column="item" :row="params.row" :index="params.$index"></v-slot>
-               </template>
-            </el-table-column>
+   <div>
+      <el-table :row-class-name="rowClassName" :border="border" :data="tableData"
+         @sort-change="orderMethod" :show-header="showHeader" :height="height">
+         <template v-for="(item) in columns">
+            <template v-if="item.render">
+               <el-table-column v-if="item.renderHeader"
+                  :prop="item.key"
+                  :sortable="item.sortable?item.sortable:false"
+                  :key="item.key" :align="item.align"
+                  :width="item.width"
+                  :render-header="item.renderHeader"
+                  :class-name="item.className">
+                  <template slot-scope="params">
+                     <v-slot :render="item.render" :column="item" :row="params.row" :index="params.$index"></v-slot>
+                  </template>
+               </el-table-column>
+
+               <el-table-column v-else
+                  :prop="item.key"
+                  :label="item.title"
+                  :sortable="item.sortable?item.sortable:false"
+                  :key="item.key"
+                  :align="item.align"
+                  :width="item.width"
+                  :class-name="item.className">
+                  <template slot-scope="params">
+                     <v-slot :render="item.render" :column="item" :row="params.row" :index="params.$index"></v-slot>
+                  </template>
+               </el-table-column>
+            </template>
 
             <el-table-column v-else
                :prop="item.key"
                :label="item.title"
-               :sortable="item.sortable?item.sortable:false"
                :key="item.key"
+               :sortable="item.sortable?item.sortable:false"
                :align="item.align"
                :width="item.width"
-               :class-name="item.className">
-               <template slot-scope="params">
-                  <v-slot :render="item.render" :column="item" :row="params.row" :index="params.$index"></v-slot>
-               </template>
+               :render-header="item.renderHeader"
+               :class-name="item.className"
+               show-overflow-tooltip>
             </el-table-column>
          </template>
+      </el-table>
+      <div v-if="showPagination" style="margin-top: 20px;text-align: right;">
+         <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="pageIndex"
+            :page-sizes="pageSizes"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total">
+         </el-pagination>
+      </div>
+   </div>
 
-         <el-table-column v-else
-            :prop="item.key"
-            :label="item.title"
-            :key="item.key"
-            :sortable="item.sortable?item.sortable:false"
-            :align="item.align"
-            :width="item.width"
-            :render-header="item.renderHeader"
-            :class-name="item.className"
-            show-overflow-tooltip>
-         </el-table-column>
-      </template>
-   </el-table>
 </template>
 <script>
 import vSlot from './SlotRender';
@@ -65,7 +79,27 @@ export default {
          type: Boolean,
          default: true
       },
-      height: String
+      height: String,
+      pageIndex: {
+         type: Number,
+         default: 1
+      },
+      pageSizes: {
+         type: Array,
+         default: () => [10, 20, 30, 40, 50]
+      },
+      pageSize: {
+         type: Number,
+         default: 10
+      },
+      total: {
+         type: Number,
+         default: 1
+      },
+      showPagination: {
+         type: Boolean,
+         default: true
+      }
    },
    data() {
       return {}
@@ -79,6 +113,14 @@ export default {
             key: params.column.property
          }
          this.$emit('orderMethod', column)
+      },
+      handleSizeChange(val) {
+        this.$emit('update:pageSize', val) // 用于支持.sync修饰符
+        this.$emit('pageSizeChange', val)
+      },
+      handleCurrentChange(val) {
+        this.$emit('update:pageIndex', val)
+        this.$emit('pageChange', val)
       }
    }
 }
