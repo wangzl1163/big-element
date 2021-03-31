d789fc64fb7180f1d08c8796c90447e82a31c173
王占领 <wangzhanling@ctfo.com>
2021-03-31 12:50:31 +0800

    echarts封装

---
+++ ECharts/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <VECharts :ref="echartsId" :option="option" :update-options="updateOptions" />
+</template>
+
+<script>
+import VECharts from './echarts.js'
+export default {
+  components: { VECharts },
+  props: {
+    option: {
+      type: Object,
+      default: () => ({})
+    },
+    updateOptions: {
+      type: Object,
+      default: () => ({
+        notMerge: true // echarts options 不合并
+      })
+    }
+  },
+  data() {
+    return {
+      echartsId: 'echarts_' + Date.now()
+    }
+  },
+  mounted() {
+    this.$nextTick(() => this.$refs[this.echartsId].resize())
+    window.onresize = () => {
+      this.$refs[this.echartsId].resize()
+    }
+  }
+}
+</script>
