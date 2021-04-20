36012379ff48b62526a06a7ffb4ef1a739f1d123
wangzl1163 <wangzl1163@qq.com>
2021-04-20 11:26:02 +0800

    修复有无数据判断bug

--- ECharts/index.vue
+++ ECharts/index.vue
@@ -1,56 +1,66 @@
 <template>
-  <div class="echarts">
-    <VECharts v-if="hasData" :ref="echartsId" :option="option" :update-options="updateOptions" :autoresize="autoresize" :loading="loading" />
-    <div v-else class="echarts_empty">{{ empty }}</div>
-  </div>
+	<div class="echarts">
+		<VECharts
+			v-if="hasData"
+			:ref="echartsId"
+			:option="option"
+			:update-options="updateOptions"
+			:autoresize="autoresize"
+			:loading="loading"
+		/>
+		    
+		<div v-else class="echarts_empty">{{ empty }}</div>
+		  
+	</div>
 </template>
 
 <script>
-import VECharts from './echarts.js'
-export default {
-  components: { VECharts },
-  props: {
-    option: {
-      type: Object,
-      default: () => ({})
-    },
-    updateOptions: {
-      type: Object,
-      default: () => ({
-        notMerge: true // echarts options 不合并
-      })
-    },
-    autoresize: { // 根据组件大小变化是否自动重绘
-      type: Boolean,
-      default: true
-    },
-    empty: {
-      type: String,
-      default: '暂无数据'
-    }
-  },
-  data() {
-    return {
-      echartsId: 'echarts_' + Date.now()
-    }
-  },
-  computed: {
-    hasData: function() {
-      // 在option被赋值为非空对象之前不做是否有数据判断
-      if (this.option.series) {
-        return this.option.series.length && this.option.series.every(item => item.data.length > 0)
-      }
+import VECharts from './echarts.js'
+export default {
+	components: { VECharts },
+	props: {
+		option: {
+			type: Object,
+			default: () => ({})
+		},
+		updateOptions: {
+			type: Object,
+			default: () => ({
+				notMerge: true // echarts options 不合并
+			})
+		},
+		autoresize: {
+			// 根据组件大小变化是否自动重绘
+			type: Boolean,
+			default: true
+		},
+		empty: {
+			type: String,
+			default: '暂无数据'
+		}
+	},
+	data() {
+		return {
+			echartsId: 'echarts_' + Date.now()
+		}
+	},
+	computed: {
+		hasData: function () {
+			// 在option被赋值为非空对象之前不做是否有数据判断
+			if (this.option.series) {
+				return this.option.series.length && this.option.series.some((item) => item.data.length > 0)
+			}
 
-      return true
-    },
-    loading: function() {
-      // 判断是否为加载数据中，第一次加载数据后再加载数据时可以把option置为空对象来实现显示加载动画
-      return !this.option.series
-    }
-  }
+			return true
+		},
+		loading: function () {
+			// 判断是否为加载数据中，第一次加载数据后再加载数据时可以把option置为空对象来实现显示加载动画
+			return !this.option.series
+		}
+	}
 }
 </script>
-<style lang="less" scoped>
+<style lang="less" scoped>
 	.echarts{
 	   width: 100%;
       height: 100%;
