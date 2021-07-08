c867d57ef725cc25066dc7ef1cd5c3c05d05004a
wangzl1163 <wangzl1163@qq.com>
2021-07-08 18:50:46 +0800

    修复有数据仍展示无数据的提示

--- ECharts/index.vue
+++ ECharts/index.vue
@@ -1,16 +1,17 @@
 <template>
 	<div class="echarts">
 		<VECharts
-			v-if="hasData"
-			:ref="echartsId"
-			:option="option"
+			v-if="hasData || showChartWidthNoData"
+			:ref="chartId"
+			:option="chartOption"
 			:update-options="updateOptions"
 			:autoresize="autoresize"
 			:loading="loading"
 		/>
 		    
-		<div v-else class="echarts_empty">{{ empty }}</div>
-		  
+		<div v-else class="echarts__empty" v-html="empty"></div>
+		
+		<div v-if="!hasData && showChartWidthNoData" class="echarts__empty echarts--no-data" v-html="empty"></div>
 	</div>
 </template>
 
@@ -20,6 +21,7 @@ export default {
 	components: { VECharts },
 	props: {
 		option: {
+			required: true,
 			type: Object,
 			default: () => ({})
 		},
@@ -37,11 +39,16 @@ export default {
 		empty: {
 			type: String,
 			default: '暂无数据'
+		},
+		showChartWidthNoData: {
+			type: Boolean, // 无数据时是否显示图表，true：显示，false：不显示
+			default: false
 		}
 	},
 	data() {
 		return {
-			echartsId: 'echarts_' + Date.now()
+			chartId: 'chart_' + Date.now(),
+			chartOption: this.option
 		}
 	},
 	computed: {
@@ -57,7 +64,47 @@ export default {
 			// 判断是否为加载数据中，第一次加载数据后再加载数据时可以把option置为空对象来实现显示加载动画
 			return !this.option.series
 		}
-	}
+	},
+	watch: {
+		option: {
+			handler: function (val) {
+				this.chartOption = val
+
+				if (!this.hasData) {
+					const isLineBar = this.option.series.every(item =>
+            ["line", "bar"].includes(item.type)
+          );
+          if (isLineBar) {
+						const timeValueObj = {
+						min: 0,
+						max: 100
+						};
+						const axisSetting = {
+						category: {
+							data: ["xx", "xx", "xx"]
+						},
+						value: timeValueObj,
+						time: timeValueObj
+						};
+
+						this.chartOption.xAxis = {
+						...this.chartOption.xAxis,
+						...axisSetting[this.chartOption.xAxis.type]
+						};
+
+						this.chartOption.yAxis = {
+						...this.chartOption.yAxis,
+						...axisSetting[this.chartOption.yAxis.type],
+						splitLine: {
+							show: false
+						}
+						};
+					}
+				}
+			},
+			immediate: true
+		}
+	},
 }
 </script>
 <style lang="less" scoped>
@@ -65,7 +112,7 @@ export default {
 	   width: 100%;
       height: 100%;
   }
-  .echarts_empty{
+  .echarts__empty{
     display: inline-block;
     position: relative;
     top:50%;
@@ -73,4 +120,7 @@ export default {
     transform: translate(-50%,-50%);
     color: #909399;
   }
+	.echarts--no-data{
+		top: -50%
+	}
 </style>
