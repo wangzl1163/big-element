9251be5af71018ee49259968aa6bc0d3515c8fc8
wangzl1163 <wangzl1163@qq.com>
2021-12-07 18:11:33 +0800

    feat: 注册组件

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import {
+	defineComponent,
+	unref,
+	shallowRef,
+	toRefs,
+	watch,
+	computed,
+	inject,
+	onMounted,
+	onUnmounted,
+	h,
+	nextTick,
+	PropType,
+	watchEffect,
+	Vue2
+ } from "vue-demi";
+
+ 
---
+++ src/register.js
@@ -0,0 +1,9 @@
+// element 组件注册器
+
+import { isVue2, isVue3 } from 'vue-demi';
+
+import { Table, TableColumn, Pagination,Dialog,Button } from 'element-ui';
+
+if (isVue2) {
+	
+}
