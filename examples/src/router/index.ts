90f6760bf1c573c0f60d768be937ee2f2784562c
wangzl1163 <wangzl1163@qq.com>
2022-01-20 17:18:17 +0800

    refactor：重构示例路由

--- examples/src/router/index.ts
+++ examples/src/router/index.ts
@@ -13,6 +13,11 @@ const router = createRouter({
 			meta: {title: 'Home'}
 		},
 		{
+			path: '/confirmButton',
+			component: () => import('@/views/ConfirmButton/index.vue'),
+			meta: {title: 'ConfirmButton'}
+		},
+		{
 			path: '/dialog',
 			component: () => import('@/views/Dialog/index.vue'),
 			meta: {title: 'Dialog'}
@@ -21,11 +26,6 @@ const router = createRouter({
 			path: '/table',
 			component: () => import('@/views/Table/index.vue'),
 			meta: {title: 'Table'}
-		},
-		{
-			path: '/confirmButton',
-			component: () => import('@/views/ConfirmButton/index.vue'),
-			meta: {title: 'ConfirmButton'}
 		}
 	]
 })
