8f17b0eaf928bb315f04138c359249f7df82794f
wangzl1163 <wangzl1163@qq.com>
2022-08-11 15:06:34 +0800

    fix：修复eslint不提示

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,16 @@
-const path = require('path')
-
 module.exports = {
-   env: {
-      browser: true,
-      node: true,
-      es2021: true
-   },
-   extends: [
-      'eslint:recommended',
-      'prettier'
-   ],
-   plugins: [
-      '@babel'
-   ],
-   parser: 'vue-eslint-parser',
-   parserOptions: {
-      sourceType: "module"
-   },
-   rules: {
-      indent: ['error', 3],
-      'no-tabs': ['error', { allowIndentationTabs: true }]
-   }
+	env: {
+		browser: true,
+		node: true,
+		es2021: true
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	rules: {
+		indent: ['error', 3],
+		'no-tabs': ['error', { allowIndentationTabs: true }]
+	}
 }
