1dd487216ed79f339ecf4d0faac486b24ac90f21
wangzl1163 <wangzl1163@qq.com>
2023-01-17 15:02:42 +0800

    feat：dialog组件对element-plus的兼容性调整为element-plus：v2.2.28以上。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		es2021: true
-	},
-	extends: ['eslint:recommended', 'prettier'],
-	parser: 'vue-eslint-parser',
-	parserOptions: {
-		sourceType: 'module'
-	},
-	rules: {
-		indent: ['error', 3],
-		'no-tabs': ['error', { allowIndentationTabs: true }]
-	}
+   env: {
+      browser: true,
+      node: true,
+      es2021: true
+   },
+   extends: ['eslint:recommended', 'prettier'],
+   parser: 'vue-eslint-parser',
+   parserOptions: {
+      sourceType: 'module'
+   },
+   rules: {
+      indent: ['error', 3],
+      'no-tabs': ['error', { allowIndentationTabs: true }]
+   }
 }
--- packages/Components/Dialog/src/index.vue
+++ packages/Components/Dialog/src/index.vue
@@ -1,34 +1,33 @@
 <template>
-	<el-dialog
-		v-bind="attrs"
-		:title="title"
-		:append-to-body="appendToBody"
-		:width="dialogWidth"
-		:close-on-click-modal="maskCloseable"
-		:show-close="showClose"
-		:customClass="'be-dialog ' + customClass"
-		@close="close"
-		@open="open"
-		@closed="closed"
-	>
-		<template v-slot:title>
-			<slot name="title"></slot>
-		</template>
+   <el-dialog
+      v-bind="attrs"
+      :title="title"
+      :append-to-body="appendToBody"
+      :width="dialogWidth"
+      :close-on-click-modal="maskCloseable"
+      :show-close="showClose"
+      @close="close"
+      @open="open"
+      @closed="closed"
+   >
+      <template v-if="vueVersion.isVue2()" v-slot:title>
+         <slot name="title"></slot>
+      </template>
 
-		<template v-slot:header>
-			<slot name="header"></slot>
-		</template>
+      <template v-if="vueVersion.isVue3()" v-slot:header>
+         <slot name="header"></slot>
+      </template>
 
-		<slot></slot>
+      <slot></slot>
 
-		<template v-slot:footer v-if="$slots.footer">
-			<slot name="footer"></slot>
-		</template>
-		<template v-slot:footer v-else-if="showFooter">
-			<el-button v-if="showCancel" @click="cancel">{{ cancelText }}</el-button>
-			<el-button type="primary" :loading="loading" @click="confirm">{{ confirmText }}</el-button>
-		</template>
-	</el-dialog>
+      <template v-slot:footer v-if="$slots.footer">
+         <slot name="footer"></slot>
+      </template>
+      <template v-slot:footer v-else-if="showFooter">
+         <el-button v-if="showCancel" @click="cancel">{{ cancelText }}</el-button>
+         <el-button type="primary" :loading="loading" @click="confirm">{{ confirmText }}</el-button>
+      </template>
+   </el-dialog>
 </template>
 <script>
 import { vueVersion } from '../../../Store/index'
@@ -36,111 +35,116 @@ import { isValidWidthUnit } from '../../../Utils/validators'
 import { isNumber } from '../../../Utils/util'
 
 export default {
-	name: 'BeDialog',
-	props: {
-		visible: {
-			required: true,
-			type: Boolean
-		},
-		title: {
-			type: String,
-			default: ''
-		},
-		width: {
-			type: [String, Number],
-			default: '550px',
-			validator: isValidWidthUnit
-		},
-		maskCloseable: {
-			type: Boolean,
-			default: false
-		},
-		showClose: {
-			type: Boolean,
-			default: true
-		},
-		appendToBody: {
-			type: Boolean,
-			default: true
-		},
-		customClass: {
-			type: String,
-			default: ''
-		},
-		showFooter: {
-			// footer 非 slot 时是否显示内置的 footer
-			type: Boolean,
-			default: true
-		},
-		showCancel: {
-			// footer 非 slot 时生效
-			type: Boolean,
-			default: true
-		},
-		confirmText: {
-			// footer 非 slot 时生效
-			type: String,
-			default: '确定'
-		},
-		cancelText: {
-			// footer 非 slot 时生效
-			type: String,
-			default: '取消'
-		}
-	},
-	// vue2 会自动忽略该选项
-	emits: ['update:visible', 'confirm', 'cancel', 'open', 'closed'],
-	data() {
-		return {
-			loading: false
-		}
-	},
-	computed: {
-		attrs() {
-			if (vueVersion.isVue2()) {
-				return {
-					...this.$attrs,
-					visible: this.visible
-				}
-			}
+   name: 'BeDialog',
+   props: {
+      visible: {
+         required: true,
+         type: Boolean
+      },
+      title: {
+         type: String,
+         default: ''
+      },
+      width: {
+         type: [String, Number],
+         default: '550px',
+         validator: isValidWidthUnit
+      },
+      maskCloseable: {
+         type: Boolean,
+         default: false
+      },
+      showClose: {
+         type: Boolean,
+         default: true
+      },
+      appendToBody: {
+         type: Boolean,
+         default: true
+      },
+      customClass: {
+         type: String,
+         default: ''
+      },
+      showFooter: {
+         // footer 非 slot 时是否显示内置的 footer
+         type: Boolean,
+         default: true
+      },
+      showCancel: {
+         // footer 非 slot 时生效
+         type: Boolean,
+         default: true
+      },
+      confirmText: {
+         // footer 非 slot 时生效
+         type: String,
+         default: '确定'
+      },
+      cancelText: {
+         // footer 非 slot 时生效
+         type: String,
+         default: '取消'
+      }
+   },
+   // vue2 会自动忽略该选项
+   emits: ['update:visible', 'confirm', 'cancel', 'open', 'closed'],
+   data() {
+      return {
+         loading: false
+      }
+   },
+   computed: {
+      attrs() {
+         if (vueVersion.isVue2()) {
+            return {
+               ...this.$attrs,
+               visible: this.visible,
+               customClass: 'be-dialog ' + this.customClass
+            }
+         }
 
-			if (vueVersion.isVue3()) {
-				return {
-					...this.$attrs,
-					modelValue: this.visible
-				}
-			}
-		},
-		dialogWidth() {
-			return isNumber(this.width) ? this.width + 'px' : this.width
-		}
-	},
-	watch: {
-		visible() {
-			this.loading = false
-		}
-	},
-	methods: {
-		confirm() {
-			this.$emit('confirm', (isLoading = false) => {
-				this.loading = isLoading
-			})
-		},
-		cancel() {
-			this.$emit('update:visible', false)
-			this.$emit('cancel')
-		},
-		open() {
-			this.$emit('open')
-		},
-		close() {
-			this.$emit('update:visible', false)
-			this.$emit('close')
-		},
-		closed() {
-			this.$emit('update:visible', false)
-			this.$emit('closed')
-		}
-	}
+         if (vueVersion.isVue3()) {
+            return {
+               ...this.$attrs,
+               modelValue: this.visible,
+               class: 'be-dialog ' + this.customClass
+            }
+         }
+      },
+      dialogWidth() {
+         return isNumber(this.width) ? this.width + 'px' : this.width
+      },
+      vueVersion() {
+         return vueVersion
+      }
+   },
+   watch: {
+      visible() {
+         this.loading = false
+      }
+   },
+   methods: {
+      confirm() {
+         this.$emit('confirm', (isLoading = false) => {
+            this.loading = isLoading
+         })
+      },
+      cancel() {
+         this.$emit('update:visible', false)
+         this.$emit('cancel')
+      },
+      open() {
+         this.$emit('open')
+      },
+      close() {
+         this.$emit('update:visible', false)
+         this.$emit('close')
+      },
+      closed() {
+         this.$emit('update:visible', false)
+         this.$emit('closed')
+      }
+   }
 }
 </script>
