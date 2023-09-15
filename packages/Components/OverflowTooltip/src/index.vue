6772c838ea2f19aeffeccf5670d509718d2d31a1
wangzl1163 <wangzl1163@qq.com>
2023-09-15 10:58:01 +0800

    fix: 修复 OverflowTooltip 功能

--- packages/Components/OverflowTooltip/src/index.vue
+++ packages/Components/OverflowTooltip/src/index.vue
@@ -12,49 +12,44 @@
 import { isNumber } from '../../../Utils/util';
 
 export default {
-	name: 'BeOverflowTooltip',
-	props: {
-		content: [String, Number, Boolean],
-		maxWidth: {
-			type: [Number, String]
-		}
-	},
-	data() {
-		return {
-			id: Math.random().toString(36).slice(2),
-			tooltipContent: '',
-			tooltipVisible: false,
-			offset: 0
-		}
-	},
+   name: 'BeOverflowTooltip',
+   props: {
+      content: [String, Number, Boolean],
+      maxWidth: {
+         type: [Number, String]
+      }
+   },
+   data() {
+      return {
+         id: Math.random().toString(36).slice(2),
+         tooltipContent: '',
+         tooltipVisible: false
+      }
+   },
 
-	computed: {
-		realMaxWidth(){
-			return isNumber(this.maxWidth) ? this.maxWidth + 'px' : this.maxWidth
-		}
-	},
+   computed: {
+      realMaxWidth(){
+         return isNumber(this.maxWidth) ? this.maxWidth + 'px' : this.maxWidth
+      }
+   },
 
-	created() {
-		this.timerId = undefined
-	},
+   methods: {
+      $_handleMouseEnter() {
+         const el = document.getElementById(this.id)
+         const elComputed = document.defaultView.getComputedStyle(el, '')
+         const padding = parseInt(elComputed.paddingLeft.replace('px', '')) + parseInt(elComputed.paddingRight.replace('px', ''))
 
-	methods: {
-		$_handleMouseEnter() {
-			const el = document.getElementById(this.id)
-			const elComputed = document.defaultView.getComputedStyle(el, '')
-			const padding = parseInt(elComputed.paddingLeft.replace('px', '')) + parseInt(elComputed.paddingRight.replace('px', ''))
+         const range = document.createRange()
+         range.setStart(el, 0)
+         range.setEnd(el, el.childNodes.length)
+         const rangeWidth = range.getBoundingClientRect().width
+         const realWidth = rangeWidth + padding
 
-			const range = document.createRange()
-			range.setStart(el, 0)
-			range.setEnd(el, el.childNodes.length)
-			const rangeWidth = range.getBoundingClientRect().width
-			const realWidth = rangeWidth + padding
-			if ((realWidth > el.offsetWidth || el.scrollWidth > el.offsetWidth) && this.$refs.tooltip) {
-				this.offset = el.offsetWidth / 2 //realWidth - el.offsetWidth > realWidth / 2 ? el.offsetWidth / 2 :
-				this.tooltipContent = this.content
-				this.tooltipVisible = true
-			}
-		}
-	}
+         if ((realWidth > parseInt(this.realMaxWidth.replace('px',''))) && this.$refs.tooltip) {
+            this.tooltipContent = this.content
+            this.tooltipVisible = true
+         }
+      }
+   }
 }
 </script>
